f38d955cdb5fd7f1709e1f6240f0a6a71f7ac90b
TornSokly <soklytorn.lyrupp@gmail.com>
2016-08-01 22:07:47 -0700

    Nothing

---
+++ src/main/java/org/khmeracademy/epseeker/ExpPpUiApplication.java
@@ -0,0 +1,12 @@
+package org.khmeracademy.epseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpPpUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpPpUiApplication.class, args);
+	}
+}
